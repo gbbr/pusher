2d453f86042f30fa29ab14c14d08c3ab9004d903
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-07-20 00:01:35 +0100

    No more channels

--- server/broadcast/broadcast.go
+++ server/broadcast/broadcast.go
@@ -8,74 +8,36 @@ import (
 
 // Initializes a new server
 func New() *Server {
-	return &Server{make([]Client, 0, 10), make(chan string)}
+	return &Server{make([]*websocket.Conn, 0, 10)}
 }
 
-/*
-CLIENT
-Listens to incoming messages and sends them to socket
-*/
-type Client struct {
-	connection *websocket.Conn
-	terminate  chan bool
-}
-
-// Send back via socket when incoming message is available
-func (client *Client) ListenTo(incoming chan string) {
-LISTENER_LOOP:
-	for {
-		select {
-		case msg := <-incoming:
-			websocket.Message.Send(client.connection, msg)
-		case <-client.terminate:
-			break LISTENER_LOOP
-		}
-	}
-}
-
-// Terminate connection and trigger breaking of listener
-func (client *Client) Terminate() {
-	client.connection.Close()
-	client.terminate <- true
-}
-
-/*
-SERVER
-Receives connections, creates new clients and feeds incoming messages
-to communication channel
-*/
 type Server struct {
-	connections []Client
-	broadcast   chan string
+	connections []*websocket.Conn
 }
 
-// Listen to WebSocket connections and register clients to
-// communication channels
+// Start server on give URL
 func (server *Server) Start(path string) {
 	onConnected := func(ws *websocket.Conn) {
 		defer ws.Close()
-		server.AddClient(Client{ws, make(chan bool)})
+		server.connections = append(server.connections, ws)
+		server.Broadcast(ws)
 	}
 
 	http.Handle(path, websocket.Handler(onConnected))
 }
 
-// Receive message from connection and send to communication channel
-func (server *Server) AddClient(client Client) {
-	server.connections = append(server.connections, client)
-
-	go server.ReceiveFrom(client)
-	client.ListenTo(server.broadcast)
-}
-
-// Receives messages from client and broadcast
-func (server *Server) ReceiveFrom(client Client) {
+// Receives messages from client and broadcast to all
+func (server *Server) Broadcast(client *websocket.Conn) {
 	var msg string
 RECEIVE_LOOP:
 	for {
-		switch websocket.Message.Receive(client.connection, &msg) {
+		switch websocket.Message.Receive(client, &msg) {
 		case nil:
-			server.broadcast <- msg
+			for _, socket := range server.connections {
+				go func(ws *websocket.Conn) {
+					websocket.Message.Send(ws, msg)
+				}(socket)
+			}
 		case io.EOF:
 			server.RemoveClient(client)
 			break RECEIVE_LOOP
@@ -84,11 +46,11 @@ RECEIVE_LOOP:
 }
 
 // Close connection and remove from pool
-func (server *Server) RemoveClient(client Client) {
-	for i, conn := range server.connections {
-		if conn == client {
+func (server *Server) RemoveClient(socket *websocket.Conn) {
+	for i, client := range server.connections {
+		if client == socket {
 			server.connections = append(server.connections[:i], server.connections[i+1:]...)
-			client.Terminate()
+			socket.Close()
 			break
 		}
 	}
